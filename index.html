64b8f7a08c3477b6afa18a8fa0431885158e5afd
Surya <87116630+makise47@users.noreply.github.com>
2021-08-05 01:05:51 +0530

    All buttons working.

--- index.html
+++ index.html
@@ -11,9 +11,10 @@
         <div class="container">
             <h1 class="title-text">Etch a Sketch</h1>
             <div class="controls">
-                <input type="button" class="color" value="Colour">
-                <input type="button" class="eraser" value="Eraser">
-                <input type="range" min="0" max="64" value="32" step="4" class="slider" id="grid-size">
+                <input type="button" class="randomcolour" value="Random" onclick="RandomiseColour()">
+                <input type="color" class="colorpicker" id="colorpicker" value="Pick Colour" onchange="ColourPickerMode()">
+                <input type="button" class="eraser" value="Eraser" onclick="Eraser()">
+                <input type="range" min="0" max="32" value="16" step="4" class="slider" id="grid-size">
                 
             </div>
 
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,46 @@
 let slider = document.getElementById("grid-size")
 let grid = document.getElementById('grid')
+let mode = 0
+// mode = 0 for random colours, mode = 1 for eraser, mode = 2 for colour picker
+let colourpickervalue
+
+DefaultGrid()
 
 function clearGrid() {
     grid.innerHTML = ''
 }
 
+function DefaultGrid() {
+    ChangeGrid(16)
+}
+
+
 slider.oninput = function() {
     let gridvalue = slider.value
     console.log(gridvalue)
+    document.getElementById("grid").innerHTML = ""
+    ChangeGrid(gridvalue)
+}
+
+function RandomiseColour() {
+    mode = 0
+}
+
+function Eraser() {
+    mode = 1
+}
+
+function ColourPickerMode() {
+    mode = 2
+    colourpickervalue = document.getElementById("colorpicker").value
+}
+
+function ChangeGrid (gridvalue) {
     grid.style.gridTemplateColumns = `repeat(${gridvalue}, 1fr)`
     grid.style.gridTemplateRows = `repeat(${gridvalue}, 1fr)`
     for (let i = 0; i<(gridvalue*gridvalue); i++) {
         let gridElement = document.createElement('div')
-        gridElement.addEventListener("mouseover", ChangeColor);
+        gridElement.addEventListener("mouseover", ChangeColor)
         gridElement.style.borderwidth = "1px"
         gridElement.style.bordercolor = "rgb(220, 220, 220, 0.25)"
         grid.appendChild(gridElement)
@@ -20,5 +48,13 @@ slider.oninput = function() {
 }
 
 function ChangeColor (e) {
-    e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    if (mode == 0) {
+        e.target.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`
+    }
+    else if (mode == 1) {
+        e.target.style.backgroundColor = `rgb(255, 255, 255)`
+    }
+    else if (mode ==2) {
+        e.target.style.backgroundColor = colourpickervalue
+    }
 }
